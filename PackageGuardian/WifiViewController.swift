309d275db1cad78971faff8f97d98c5277ad4bc5
WXS <xwang152@stevens.edu>
2019-10-16 18:14:40 -0400

    Add passing address coordinates function

--- PackageGuardian/WifiViewController.swift
+++ PackageGuardian/WifiViewController.swift
@@ -7,7 +7,10 @@
 //
 
 import UIKit
+import CoreLocation
 class WifiViewController: ViewController {
+    
+    @IBOutlet weak var addressSearch: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,17 +20,51 @@ class WifiViewController: ViewController {
     
 
     @IBAction func connect(_ sender: Any) {
-        mqttClient.connect()
-        mqttClient.publish("rpi/gpio", withString: "on")
-    }
-    /*
-    // MARK: - Navigation
+        let geoCoder = CLGeocoder()
+        guard
+            let address = self.addressSearch.text
+        else{
+            let alert = UIAlertController(title: "Alert", message: "Please enter the address!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+                  switch action.style{
+                  case .default:
+                        print("default")
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+                  case .cancel:
+                        print("cancel")
+
+                  case .destructive:
+                        print("destructive")
+
+
+            }}))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        geoCoder.geocodeAddressString(address) { (placemarks, error) in
+            guard
+                let placemarks = placemarks,
+                let lat = placemarks.first?.location?.coordinate.latitude,
+                let log = placemarks.first?.location?.coordinate.longitude
+            else {
+                // handle no location found
+                self.alert(message: "No location found!")
+                return
+            }
+            print(lat, log)
+            mqttClient.connect()
+            mqttClient.publish("rpi/gpio", withString: "on")
+            mqttClient.publish("gps/lat", withString: String(format:"%f", lat))
+            mqttClient.publish("gps/log", withString: String(format:"%f", log))
+            self.alert(message: "Location \(lat), \(log) has been sent and GPS is activated!")
+        }
+        
+        
     }
-    */
+    func alert(message:String){
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 
 }
