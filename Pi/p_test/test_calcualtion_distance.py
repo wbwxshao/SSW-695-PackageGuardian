dcd24f26e3111bc7f64178170682670d1bb4bb10
Sherman77 <44691915+Sherman77@users.noreply.github.com>
2019-11-09 00:03:10 -0500

    Added new test case

--- Pi/p_test/test_calcualtion_distance.py
+++ Pi/p_test/test_calcualtion_distance.py
@@ -6,7 +6,7 @@ from server import calculateDistance
 class TestCalculationDistance(unittest.TestCase):
     """unit test for function calculationDistance"""
 
-    def test_calculationDistance_1(self):
+    def test_calculationDistance(self):
         """test case 1"""
         self.assertEqual(calculateDistance(40.7448, 74.0256), "The distance between current location and *** is 0 m. ")
 
