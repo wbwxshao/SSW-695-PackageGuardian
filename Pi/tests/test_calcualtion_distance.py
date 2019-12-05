398eaca702d13502db7ddab4caec9cee84cd9cb3
Sherman77 <44691915+Sherman77@users.noreply.github.com>
2019-12-05 16:02:44 -0500

    Update test_calcualtion_distance.py

--- Pi/tests/test_calcualtion_distance.py
+++ Pi/tests/test_calcualtion_distance.py
@@ -8,7 +8,7 @@ class TestCalculationDistance(unittest.TestCase):
 
     def test_calculationDistance(self):
         """test case 1"""
-        self.assertEqual(calculateDistance(40.7448, 74.0256), "The distance between current location and *** is 0 m. ")
+        self.assertEqual(calculateDistance(40.7448, 74.0256), 0)
 
 if __name__ == '__main__':
     unittest.main(exit = False, verbosity = 2)
